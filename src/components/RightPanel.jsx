f37723621e9d5da5d06d291f49b90d279ed4c8f7
Debjit Biswas <debjit@fastmail.fm>
2024-11-13 12:05:11 +0530

    feat: Show hide right panel support

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -13,19 +13,34 @@ import { KeyboardShortcuts } from "./KeyboardShortcuts";
 
 export function RightPanel() {
   const [activeTab, setActiveTab] = createSignal('cpu');
+  const [expanded, setExpanded] = createSignal(true);
+
+  const toggleExpanded = () => {
+    setExpanded((expanded) => !expanded);
+  };
+
+  const showTab = (tab) => {
+    if (tab === activeTab()) {
+      toggleExpanded();
+    } else {
+      setExpanded(true);
+    }
+
+    setActiveTab(tab);
+  };
 
   return (
-    <div class="flex items-start w-full">
-      <div class="flex flex-col items-center h-sm:gap-4 gap-8 px-4 pt-4 border-r border-r-gray-300 dark:border-r-gray-600" style={{ height: "calc(100vh - 6rem)" }}>
-        <button type="button" onClick={() => setActiveTab('cpu')} class={`${activeTab() === 'cpu' ? 'text-blue-600 dark:text-blue-400' : ''} flex flex-col items-center`}>
+    <div class={`flex items-start ${expanded() ? "w-[25vw] min-w-[295px] flex-shrink-0" : ""}`}>
+      <div class="relative z-10 dark:bg-gray-900 flex flex-col items-center h-sm:gap-4 gap-8 px-4 pt-4 border-r border-r-gray-300 dark:border-r-gray-600" style={{ height: "calc(100vh - 6rem)" }}>
+        <button type="button" onClick={() => showTab('cpu') } class={`${activeTab() === 'cpu' && expanded() ? 'text-blue-600 dark:text-blue-400' : ''} flex flex-col items-center`}>
           <FiCpu class="text-2xl"/>
               {/*<span>CPU</span>*/}
         </button>
-        <button type="button" onClick={() => setActiveTab('memory')} class={`${activeTab() === 'memory' ? 'text-blue-600 dark:text-blue-400' : ''} flex flex-col items-center`}>
+        <button type="button" onClick={() => showTab('memory')} class={`${activeTab() === 'memory' && expanded() ? 'text-blue-600 dark:text-blue-400' : ''} flex flex-col items-center`}>
           <BsMemory class="text-2xl" />
             {/*<span>Memory</span>*/}
         </button>
-        <button type="button" onClick={() => setActiveTab('io')} class={`${activeTab() === 'io' ? 'text-blue-600 dark:text-blue-400' : ''} flex flex-col items-center`}>
+        <button type="button" onClick={() => showTab('io')} class={`${activeTab() === 'io' && expanded() ? 'text-blue-600 dark:text-blue-400' : ''} flex flex-col items-center`}>
           <p class="text-md font-bold flex gap-[-1]">
             <span class="text-nowrap whitespace-nowrap">I/O</span>
             {/* <BsSlash class="text-2xl"/> */}
@@ -38,12 +53,13 @@ export function RightPanel() {
         <button type="button" class="hidden">
           <AiOutlineQuestionCircle class="text-xl"/>
         </button>
-        <button type="button" class="hidden">
-          <BsKeyboard class="text-xl"/>
-        </button>
         <div class="pb-1"></div>
       </div>
-      <div class="min-w-60 w-full bg-gray-100 dark:bg-gray-800 p-4 h-full flex overflow-x-hidden overflow-y-auto" style={{ height: "calc(100vh - 6rem)" }}>
+      <div id="content" class="relative z-5 min-w-60 w-full bg-gray-100 dark:bg-gray-800 p-4 h-full flex overflow-x-hidden overflow-y-auto transform transition-transform duration-300 ease-in-out"
+        style={{
+          height: "calc(100vh - 6rem)",
+          display: expanded() ? "flex" : "none",
+        }}>
         <div class={`w-full max-h-full ${activeTab() === 'cpu' ? '' : 'hidden'}`}>
           <div>
             <Registers />
