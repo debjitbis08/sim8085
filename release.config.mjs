e74106899d46edfe3e03663078faa24c59c3a467
Debjit Biswas <debjit@fastmail.fm>
2024-11-29 11:43:58 +0530

    ci: Fix release job

--- release.config.mjs
+++ release.config.mjs
@@ -2,24 +2,24 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ["master"],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
+    branches: ["master"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+            },
+        ],
+        "@semantic-release/npm", // Updates package.json version
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+                message: "release: Release ${nextRelease.version} [skip actions]\n\n${nextRelease.notes}",
+            },
+        ],
+        "@semantic-release/github", // Creates GitHub release
     ],
-    '@semantic-release/npm', // Updates package.json version
-    [
-  		"@semantic-release/git",
-  		{
-  			"assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
-  			"message": "release: Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-  		}
-  	],
-    '@semantic-release/github', // Creates GitHub release
-  ],
 };
