a21d2be304ce9d6da2a95177f7de0d437d224194
Debjit Biswas <debjit@fastmail.fm>
2025-05-06 11:46:22 +0530

    fix: Remove supabase requirement

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,49 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export let supabase = null;
 
+if (supabaseUrl && supabaseAnonKey) {
+    supabase = createClient(supabaseUrl, supabaseAnonKey);
+}
+
+export const getUser = async () => {
+    if (!supabase) return { user: null, error: new Error("Supabse is not initialized") };
+
+    const {
+        data: { user },
+        error,
+    } = await supabase.auth.getUser();
+
+    return { user, error };
+};
+
+export const getSession = async () => {
+    if (!supabase) return { session: null, error: new Error("Supabse is not initialized") };
+
+    const {
+        data: { session },
+        error,
+    } = await supabase.auth.getSession();
+
+    return { session, error };
+};
+
+export const getUserFromSession = async () => {
+    if (!supabase) return { user: null, error: new Error("Supabse is not initialized") };
+
+    const {
+        data: { session },
+        error,
+    } = await supabase.auth.getSession();
+
+    return { user: session.user, error };
+};
+
+export const signOut = async () => {
+    if (supabase) {
+        await supabase.auth.signOut();
+    }
+};
