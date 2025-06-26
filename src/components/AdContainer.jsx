b3fb9d6f99a080092f83276fd4c02e6214ba06af
Debjit Biswas <debjit@fastmail.fm>
2025-06-26 19:49:50 +0530

    fix: Add better ad container visibility logic

--- src/components/AdContainer.jsx
+++ src/components/AdContainer.jsx
@@ -1,4 +1,4 @@
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 import { supabase, getUser } from "../lib/supabase.js";
 
 export default function AdContainer(props) {
@@ -45,19 +45,17 @@ export default function AdContainer(props) {
     };
 
     return (
-        <div>
-            {tier() === "PLUS" || tier() === "LOADING" ? null : (
-                <div class={`mt-auto relative ${props.isHidden ? "hidden" : ""}`}>
-                    {props.children}
-                    <div
-                        title="Upgrade"
-                        class="absolute top-[-10px] right-[-5px] border border-inactive-border hover:border-active-border bg-secondary-background hover:bg-main-background text-secondary-foreground hover:text-active-foreground px-2 py-0 rounded-full cursor-pointer"
-                        onClick={openPlusDialog}
-                    >
-                        &times;
-                    </div>
+        <Show when={tier() === "FREE"}>
+            <div class={`mt-auto relative ${props.isHidden ? "hidden" : ""}`}>
+                {props.children}
+                <div
+                    title="Upgrade"
+                    class="absolute top-[-10px] right-[-5px] border border-inactive-border hover:border-active-border bg-secondary-background hover:bg-main-background text-secondary-foreground hover:text-active-foreground px-2 py-0 rounded-full cursor-pointer"
+                    onClick={openPlusDialog}
+                >
+                    &times;
                 </div>
-            )}
-        </div>
+            </div>
+        </Show>
     );
 }
